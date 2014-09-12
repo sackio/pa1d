bfbfe0b2f47ca5b0dcb96ac8402fd11b62c961d0
Ben Sack <bsack@pushbuild.com>
2014-09-12 13:15:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# pa1d
+
+One payment module to rule them all
+
+## Getting Started
+Install the module with: `npm install pa1d`
+
+```javascript
+var pa1d = require('pa1d');
+```
+
+## License
+Copyright (c) 2014 Ben Sack  
+Licensed under the MIT license.
---
+++ test/pa1d_test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var pa1d = require('../lib/pa1d.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['pa1d'] = {
+  setUp: function(done) {
+    done();
+  },
+  'braintree': function(test) {
+    test.ok(true);
+    test.done();
+  },
+};
